c469248654e1879a0275aa70d9a17c2fb15a9432
Student <Student@PC95>
2017-11-06 10:12:40 +0800

    Done

---
+++ src/practical/pkg1b/extra/Practical1BExtra.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical.pkg1b.extra;
+
+/**
+ *
+ * @author TARUC
+ */
+public class Practical1BExtra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String expr = "25.3 54.9 + 3.5 2.0 * /";
+        String[] tokens = expr.split(" ");
+        System.out.println("expr = " + expr);
+        for(int i = 0; i < tokens.length; ++i){
+            
+        }
+        
+    }
+    
+}
